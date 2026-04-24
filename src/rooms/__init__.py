055abe8e8042a8f75b7fce8eaf10fa352c401609
Phillon Morris <phillon.morris@gmail.com>
2026-04-24 06:53:11 +0100

    feat: export room events and artifact sync APIs

--- src/rooms/__init__.py
+++ src/rooms/__init__.py
@@ -12,6 +12,15 @@ from .delivery_room import (
     record_room_phase_result,
     set_room_phase,
 )
+from .room_artifacts import discover_room_artifacts, sync_room_artifacts
+from .room_events import (
+    RoomEvent,
+    RoomEventType,
+    append_room_event,
+    export_room_events_markdown,
+    filter_room_events,
+    load_room_events,
+)
 from .room_runner import run_delivery_room
 from .room_state import (
     DeliveryRoomState,
@@ -28,16 +37,24 @@ __all__ = [
     "RoomArtifact",
     "RoomBlocker",
     "RoomDecision",
+    "RoomEvent",
+    "RoomEventType",
     "RoomHandoff",
     "add_room_blocker",
     "add_room_decision",
     "add_room_handoff",
+    "append_room_event",
     "create_delivery_room",
+    "discover_room_artifacts",
     "export_delivery_room",
+    "export_room_events_markdown",
+    "filter_room_events",
     "get_delivery_room_status",
     "load_delivery_room",
+    "load_room_events",
     "mark_room_agent_status",
     "record_room_phase_result",
     "run_delivery_room",
     "set_room_phase",
+    "sync_room_artifacts",
 ]
